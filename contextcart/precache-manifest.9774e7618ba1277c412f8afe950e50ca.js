231545d026d4fb5ae57e4cba74dbad997df82204
agent13 <hnkn1998@gmail.com>
2019-03-28 15:18:51 +0430

    added context cart

---
+++ contextcart/precache-manifest.9774e7618ba1277c412f8afe950e50ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "215407b4bcc6184931f1",
+    "url": "/static/js/main.3603aaef.chunk.js"
+  },
+  {
+    "revision": "ff6462ed31e81181fbdc",
+    "url": "/static/js/2.7696e660.chunk.js"
+  },
+  {
+    "revision": "215407b4bcc6184931f1",
+    "url": "/static/css/main.8dfe3e80.chunk.css"
+  },
+  {
+    "revision": "b6ff14edde0527ab18676f5707ffda3b",
+    "url": "/index.html"
+  }
+];
